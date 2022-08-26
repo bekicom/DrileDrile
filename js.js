f91bbfc04d95783c020b5508f673469a9036192c
Aisha-aleova <madashaliyeva@gmail.com>
2022-08-26 16:05:43 +0500

    ygiy

---
+++ js.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector(".wrapper");
+const section4 = document.createElement("section");
+section4.classList.add("section4");
+section4.classList.add("section4");
+wrapper.append(section4);
+
