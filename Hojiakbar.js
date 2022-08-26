37fde32acdedf4f11e75f10f033d749cf99dc856
Hojiakbar2009 <108812258+Hojiakbar2009@users.noreply.github.com>
2022-08-26 16:24:40 +0500

    start

--- Hojiakbar.js
+++ Hojiakbar.js
@@ -2,7 +2,17 @@ let section_1 = document.querySelector(".section_1");
 
 const arr = [
     {
-        left: "./img/Vector(54).png"
-
+        left: "./img/Vector(54).png",
+        center: "./img/2.png",
+        malumot: "Get ready for Ourstylist chair",
+        right: "./img/Vector(55).png",
     } 
 ]
+
+arr.forEach(item => {
+    section_1.innerHTML += `
+    <div class="left">
+    <img src=${item.left}>
+    </div>
+    `
+});
