bf93d5440dfc7f0d5de5904c5a4e523a2fe106fb
Aisha-aleova <madashaliyeva@gmail.com>
2022-08-26 16:25:55 +0500

    fold

--- section4.js
+++ section4.js
@@ -3,9 +3,8 @@ const section4 = document.createElement("section");
 const title = document.createElement("p");
 const carousel = document.createElement("div");
 section4.classList.add("section4");
+title.classList.add("s4-title")
 wrapper.append(section4);
 section4.append(title);
 section4.append(carousel);
-title.innerHTML = `
-    <p>TRENDING CATEGORIES</p>
-`;
+title.innerHTML = "TRENDING CATEGORIES";
