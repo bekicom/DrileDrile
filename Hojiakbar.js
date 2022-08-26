b7862c7f2d999359448c4f7bfdc0fd73ba2e8304
Hojiakbar2009 <108812258+Hojiakbar2009@users.noreply.github.com>
2022-08-26 16:25:11 +0500

    start

--- Hojiakbar.js
+++ Hojiakbar.js
@@ -2,17 +2,18 @@ let section_1 = document.querySelector(".section_1");
 
 const arr = [
     {
-        left: "./img/Vector(54).png",
+        left: "2.png",
         center: "./img/2.png",
         malumot: "Get ready for Ourstylist chair",
-        right: "./img/Vector(55).png",
+        right: "./img/Vector(55).png"
     } 
 ]
 
 arr.forEach(item => {
-    section_1.innerHTML += `
-    <div class="left">
+    section_1.innerHTML = `
+    <div class="icon">
     <img src=${item.left}>
+    <img src=${item.center}>
     </div>
     `
 });
