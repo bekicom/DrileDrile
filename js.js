c8f230df5135ed39a5fd2ff3ab47ec068b73acc8
Aisha-aleova <madashaliyeva@gmail.com>
2022-08-26 16:12:05 +0500

    hello

--- js.js
+++ js.js
@@ -1,6 +1,2 @@
-const wrapper = document.querySelector(".wrapper");
-const section4 = document.createElement("section");
-section4.classList.add("section4");
-section4.classList.add("section4");
-wrapper.append(section4);
+
 
---
+++ section4.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector(".wrapper");
+const section4 = document.createElement("section");
+const title = document.createElement("p");
+const carousel = document.createElement("div");
+section4.classList.add("section4");
+wrapper.append(section4);
+section4.append(title);
+section4.append(carousel);
+title.innerHTML
