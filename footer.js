a289ac27339b4f61ec3cc18a71fc6c8f7e319801
BOBURJON <sahodullaevboburzon@gmail.com>
2022-08-26 16:16:40 +0500

    jhgjhg

---
+++ footer.js
@@ -0,0 +1,13 @@
+const wrapper = document.querySelector('.wrapper');
+
+const data = [
+    {
+        name: 'Drile',
+        bolim: 'ABOUT US',
+        info: 'About Us',
+        
+
+
+
+    }
+]
