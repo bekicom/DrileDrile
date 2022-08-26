9cedbdfda4b6281ebb577f6761d74c4f4369ad45
Aisha-aleova <madashaliyeva@gmail.com>
2022-08-26 16:36:20 +0500

    sf

--- section4.js
+++ section4.js
@@ -1,10 +1,7 @@
-const wrapper = document.querySelector(".wrapper");
-const section4 = document.createElement("section");
+const section4 = document.querySelector("#section4")
 const title = document.createElement("p");
 const carousel = document.createElement("div");
-section4.classList.add("section4");
 title.classList.add("s4-title")
-wrapper.append(section4);
 section4.append(title);
 section4.append(carousel);
 title.innerHTML = "TRENDING CATEGORIES";
