201b3b8f99ab77f321654f37f529561205923df7
Hojiakbar2009 <108812258+Hojiakbar2009@users.noreply.github.com>
2022-08-27 08:03:11 +0500

    m

--- sec1.js
+++ sec1.js
@@ -1,20 +1,20 @@
-// let section_1 = document.querySelector(".section_1");
+let section_1 = document.querySelector(".section_1");
 
-// const arr = [
-//     {
-//         center: "./img/2.png",
-//         malumot: "Get ready for Ourstylist chair",
-//         right: "./img/Vector(55).png"
-//     } 
-// ]
+const arr = [
+    {
+        center: "./img/2.png",
+        malumot: "Get ready for Ourstylist chair",
+        right: "./img/Vector(55).png"
+    } 
+]
 
-// arr.forEach(item => {
-//     section_1.innerHTML = `
-//     <div class="box">
-//     <img src=${item.center}>
-//     <p>${item.malumot}</p>
-//     </div>
-//     `
-// });
+arr.forEach(item => {
+    section_1.innerHTML = `
+    <div class="box">
+    <img src=${item.center}>
+    <p>${item.malumot}</p>
+    </div>
+    `
+});
 
 
